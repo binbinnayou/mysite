045a5b722101945a45c590166ada14966595d3a9
nayoubin <binbinnayou@gmail.com>
2021-03-23 17:12:01 +0900

    1

--- lib/main.dart
+++ lib/main.dart
@@ -34,9 +34,9 @@ class _SlideshowState extends State<SlidePage> {
   // データ
   List<String> _imageList = [
     "images/1.jpg",
-    "images/2.jpg",
-    "images/3.jpg",
-    "images/4.jpg",
+    "images/1.jpg",
+    "images/1.jpg",
+    "images/1.jpg",
   ];
 
 
